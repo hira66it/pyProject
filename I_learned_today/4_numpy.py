dedf4e4c6ed0b75a7881cbef22eb91ee141282c7
hira66it <hiwhale3707@naver.com>
2019-01-24 00:18:38 +0900

    I_learned_today

--- I_learned_today/4_numpy.py
+++ I_learned_today/4_numpy.py
@@ -1,1 +1,14 @@
 import numpy as np
+
+values = [[1, 1],[1,1]]
+a = np.array(values)
+
+a.shape
+print(np.dot(a,np.dot(a,a)))
+print(np.dot(a,a,a))
+
+
+m = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+print(m.shape)
+n =m.T
+print(n.shape)

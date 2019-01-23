1ad80b9fcf4b8a672b0723cf007b2c2c5d718c25
hira66it <hiwhale3707@naver.com>
2019-01-24 00:19:03 +0900

    i I_learned_today

--- I_learned_today/3_1_line.py
+++ I_learned_today/3_1_line.py
@@ -7,13 +7,15 @@ import random
 def makesample(n,mu,std):
     tmp=[]
     lst_x = np.random.normal(mu,std,n)
-    return lst_x.tolist()
+    return lst_x
 
 n=100
 x1 = makesample(n,2,1)
 x2 = makesample(n,-2,1)
 y1 = makesample(n,0,3)
 y2 = makesample(n,0,3)
+
+sigma_ml = np.dot(x1-np.mean(x1),np.transpose(x2-np.mean(x2)))
 
 plt.plot(x1, y1,'b.')
 plt.plot(x2, y2,'r.')
